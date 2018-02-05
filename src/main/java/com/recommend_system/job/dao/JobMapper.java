fcd69d21c1063381c2d468035a36c1443344f06c
raojay7 <879645920@qq.com>
2018-02-05 18:52:51 +0800

    增加了dao的用例子进行操作集的方法

--- src/main/java/com/recommend_system/job/dao/JobMapper.java
+++ src/main/java/com/recommend_system/job/dao/JobMapper.java
@@ -1,15 +1,32 @@
 package com.recommend_system.job.dao;
 
 import com.recommend_system.job.entity.Job;
+import com.recommend_system.job.entity.JobExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface JobMapper {
+    int countByExample(JobExample example);
+
+    int deleteByExample(JobExample example);
+
     int deleteByPrimaryKey(Integer jobId);
 
     int insert(Job record);
 
     int insertSelective(Job record);
 
+    List<Job> selectByExampleWithBLOBs(JobExample example);
+
+    List<Job> selectByExample(JobExample example);
+
     Job selectByPrimaryKey(Integer jobId);
+
+    int updateByExampleSelective(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByExample(@Param("record") Job record, @Param("example") JobExample example);
 
     int updateByPrimaryKeySelective(Job record);
 
