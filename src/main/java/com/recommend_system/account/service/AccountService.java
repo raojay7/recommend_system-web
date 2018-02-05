04eda574c8d5412c0d3e79236ab1288d18496d71
Wenqiang Luo <943183575@qq.com>
2018-02-05 15:27:16 +0800

    deleted wrong folder

--- src/main/java/com/recommend_system/account/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.recommend_system.account.service;
-
-import com.recommend_system.account.entity.Account;
-
-public interface AccountService {//service接口
-    public void register(Account account);
-    public Account logIn(Account account);
-}
