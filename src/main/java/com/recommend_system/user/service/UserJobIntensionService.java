0a32d2dad9ec5e4a9145ecc6a5bb831bb2f43f27
Wenqiang Luo <943183575@qq.com>
2018-02-06 21:17:06 +0800

    修改个人信息、修改和增加求职意愿servcie接口

--- src/main/java/com/recommend_system/user/service/UserJobIntensionService.java
+++ src/main/java/com/recommend_system/user/service/UserJobIntensionService.java
@@ -12,12 +12,14 @@ public interface UserJobIntensionService {
      *修改用户求职意愿
      * @param userJobIntension:传入修改后的用户求职意愿bean
      * controller中获得session中的user，根据user的id修改对应userid的userJobIntension
+     * 或者前端直接传相应的id过来
      */
     public void setIntension(UserJobIntension userJobIntension);
     /*
      *增加用户求职意愿
      * @param userJobIntension:传入用户增加的用户求职意愿bean
      * controller中获得session中的user，根据user的id新插入对应userid的userJobIntension
+     * 或者前端传id过来
      */
     public void addIntension(UserJobIntension userJobIntension);
 }
