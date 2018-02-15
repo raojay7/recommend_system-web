b2fec044888816815731a10d945e2925862bcede
raojay7 <879645920@qq.com>
2018-02-15 15:18:37 +0800

    jedis客户端交互

---
+++ src/main/java/com/recommend_system/utils/impl/JedisClientCluster.java
@@ -0,0 +1,69 @@
+package com.recommend_system.utils.impl;
+
+import com.recommend_system.utils.JedisClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+import redis.clients.jedis.JedisCluster;
+
+/**
+ * redis集群版客户端
+ * <p>Title: JedisClientCluster</p>
+ * <p>Description: </p>
+ */
+public class JedisClientCluster implements JedisClient
+{
+
+	@Autowired
+	private JedisCluster jedisCluster;
+	
+	@Override
+	public String get(String key) {
+		String string = jedisCluster.get(key);
+		return string;
+	}
+
+	@Override
+	public String set(String key, String value) {
+		String string = jedisCluster.set(key, value);
+		return string;
+	}
+
+	@Override
+	public long incr(String key) {
+		Long result = jedisCluster.incr(key);
+		return result;
+	}
+
+	@Override
+	public Long hset(String hkey, String key, String value) {
+		Long result = jedisCluster.hset(hkey, key, value);
+		return result;
+	}
+
+	@Override
+	public String hget(String hkey, String key) {
+		String string = jedisCluster.hget(hkey, key);
+		return string;
+	}
+
+	@Override
+	public Long del(String key) {
+		Long result = jedisCluster.del(key);
+		return result;
+	}
+
+	@Override
+	public Long hdel(String hkey, String key) {
+		Long result = jedisCluster.hdel(hkey, key);
+		return result;
+	}
+
+	@Override
+	public Long expire(String key, int second) {
+		Long result = jedisCluster.expire(key, second);
+		return result;
+	}
+
+}
