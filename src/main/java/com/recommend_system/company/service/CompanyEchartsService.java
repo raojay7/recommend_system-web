4618050f7a1f951a9bd24ef9a7da1b4ea902f74b
Wenqiang Luo <943183575@qq.com>
2018-02-18 22:52:56 +0800

    岗位、企业相关统计service接口的完善

--- src/main/java/com/recommend_system/company/service/CompanyEchartsService.java
+++ src/main/java/com/recommend_system/company/service/CompanyEchartsService.java
@@ -5,36 +5,42 @@ public interface CompanyEchartsService {
     /*
      *企业地域分布
      * SELECT COUNT(CITY),CITY FROM T_COMPANY GROUP BY CITY
+     * 需要map<String,Integer>
      */
-    public void getCompanyCity();
+    public String getCompanyCity();
 
     /*
      *企业行业分布
      * SELECT COMPANY_INDUSTRY,COUNT(*) FROM T_COMPANY GROUP BY COMPANY_INDUSTRY;
+     * 需要map<String,Integer>
      */
-    public void getCompanyIndustry();
+    public String getCompanyIndustry();
 
     /*
      *企业性质分布
      * SELECT COUNT(*),COMPANY_NATURE FROM T_COMPANY GROUP BY COMPANY_NATURE;
+     * 需要map<String,Integer>
      */
-    public void getCompanyNature();
+    public String getCompanyNature();
 
     /*
      *企业规模分布
      * SELECT COUNT(*),COMPANY_SCALE FROM T_COMPANY GROUP BY COMPANY_SCALE
+     * 需要map<Integer,Integer>
      */
-    public void getCompanyScale();
+    public String getCompanyScale();
 
     /*
      *不同性质公司的平均工资及岗位需求量分布
      * SELECT SUM(need_num)AS '岗位个数',c.company_nature AS '公司性质',AVG((j.salary_min+j.salary_max)/2) AS '平均薪水' FROM t_job j,t_company c WHERE j.company_id=c.company_id GROUP BY c.company_nature;
+     * 需要2个map<String,Integer>
      */
-    public void getCompanyNatureNeedAvg();
+    public String getCompanyNatureNeedAvg();
 
     /*
      *不同行业公司平均工资及岗位需求量分布
      * SELECT SUM(need_num)AS '岗位个数',c.company_industry,AVG((j.salary_min+j.salary_max)/2) AS '平均薪水' FROM t_job j,t_company c WHERE j.company_id=c.company_id  GROUP BY company_industry;
+     * 需要2个map<String,Integer>
      */
-    public void getCompanyIndustryNeedAvg();
+    public String getCompanyIndustryNeedAvg();
 }
--- src/main/java/com/recommend_system/job/service/JobEchartsService.java
+++ src/main/java/com/recommend_system/job/service/JobEchartsService.java
@@ -5,24 +5,28 @@ public interface JobEchartsService {
     /*
      *城市平均工资条形统计图
      * SELECT workcity,AVG((salary_min+SALARY_MAX)/2) FROM t_job GROUP BY workcity
+     * 需要map<String,Integer>
      */
-    public void getCityAvgSalary();
+    public String getCityAvgSalary();
 
     /*
      *学历工资分布
      * SELECT SUM(NEED_NUM),AVG((SALARY_MIN+SALARY_MAX)/2),EDUCATION FROM T_JOB GROUP BY EDUCATION;
+     * 需要map<String,Integer>
      */
-    public void getEducationAvgSalary();
+    public String getEducationAvgSalary();
 
     /*
      *职位地域分布
      * SELECT WORKCITY,COUNT(*) FROM T_JOB GROUP BY WORKCITY
+     * 需要map<String,Integer>
      */
-    public void getJobCity();
+    public String getJobCity();
 
     /*
      *工作经验工资分布
      * SELECT WORKEXPERIENCE_MIN,WORKEXPERIENCE_MAX,AVG((SALARY_MIN+SALARY_MAX)/2) FROM T_JOB GROUP BY WORKEXPERIENCE_MIN,WORKEXPERIENCE_MAX;
+     * 工作经验范围是固定的，1-3,3-5,5-10,只需要三个对应的AVG值，需要list<Integer>
      */
-    public void getExperienceSalary();
+    public String getExperienceSalary();
 }
