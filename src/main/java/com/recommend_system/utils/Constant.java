e266b6c8af00f489fca12747de6764d50120188a
raojay7 <879645920@qq.com>
2018-03-04 17:08:52 +0800

    用于图表的常量类

---
+++ src/main/java/com/recommend_system/utils/Constant.java
@@ -0,0 +1,13 @@
+package com.recommend_system.utils;
+
+public class Constant
+{
+    public static final String UNKONW_SCALE="保密";
+    public static final String VERYSMALL_SCALE="20人以下";
+    public static final String SMALL_SCALE="20-99人";
+    public static final String MIDDLE_SCALE="100-499人";
+    public static final String LARGE_SCALE="500-999人";
+    public static final String XLARGE_SCALE="1000-9999人";
+    public static final String XXLARGE_SCALE="10000人以上";
+
+}
